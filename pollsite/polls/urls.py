a77eb3535eb178ad4464b0b5a326748bd33fbca1
vktr-r2 <122321007+vktr-r2@users.noreply.github.com>
2024-03-26 07:37:05 -0400

    Refactor to use class based template views

--- pollsite/polls/urls.py
+++ pollsite/polls/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from . import views
+from .views import IndexView, DetailView, ResultsView, VoteView
 
-
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
+
+    # index of questions
+    path("", IndexView.as_view(), name="index"),
+
+    # specific question ex:/polls/5/
+    path("<int:question_id>/", DetailView.as_view(), name="detail"),
+
+    # question results ex:/polls/5/results
+    path("<int:question_id>/results/", ResultsView.as_view(), name="results"),
+
+    # question vote ex:/polls/5/vote
+    path("<int:question_id>/vote/", VoteView.as_view(), name="vote"),
+
 ]
--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,5 +1,35 @@
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question, Choice
 
 
-def index(request):
-    return HttpResponse("Hello, Vik.  You're at the polls index.")
+class IndexView(TemplateView):
+
+    def get(self, request):
+        latest_questions_list = Question.objects.order_by("-pub_date")[:5]
+        context = {"latest_questions_list": latest_questions_list}
+        return render(request, "polls/index.html", context)
+
+
+class DetailView(TemplateView):
+
+    def get(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        return render(request, "polls/detail.html", {"question": question})
+
+
+class ResultsView(TemplateView):
+
+    def get(self, request, question_id):
+        response = (f"You're looking at the results of question {question_id}.")
+        return HttpResponse(response, question_id)
+
+
+class VoteView(TemplateView):
+    
+    def post(self, request, question_id):
+        response = (f"You're voting on question {question_id}")
+        return HttpResponse(response, question_id)
